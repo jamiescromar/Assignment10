94cfb5989696305fea3322653ef7e54f2ea43a42
jamiescromar <jamiescromar@gmail.com>
2021-03-30 14:21:28 -0600

    Add project files.

---
+++ Assignment10/Components/TeamViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Components
+{
+    public class TeamViewComponent: ViewComponent
+    {
+        //create context variable
+        private BowlingLeagueContext context;
+        //Build the constructor
+        public TeamViewComponent(BowlingLeagueContext con)
+        {
+            context = con;
+        }
+        //What do you want me to do when this is called
+        public IViewComponentResult Invoke()
+        {
+            //get the team data in this invoke for the view component so we can display this
+            return View(context.Teams.Distinct().OrderBy(x=>x));
+        }
+
+    }
+}
---
+++ Assignment10/Models/ViewModels/PageNumberingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Models.ViewModels
+{
+    public class PageNumberingInfo
+    {
+        public int NumItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalNumItems { get; set; }
+
+        //Calculate the number of pages
+        //Turn that top number back into an int to make sure that we have enough pages to show all our pages
+        public int NumPages => (int) Math.Ceiling(((decimal) TotalNumItems / NumItemsPerPage));
+    }
+}
